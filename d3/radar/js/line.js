2743df5f6c7b7552434d6742a3d877c33e2d33ef
Resl411 <teresa.steinebach@web.de>
2019-11-22 13:41:24 +0100

    color coding2
    line.js

--- d3/radar/js/line.js
+++ d3/radar/js/line.js
@@ -27,7 +27,7 @@ const height = +line_svg.attr('height');
 
 const render = data => {
     line_svg.selectAll("*").remove();
-  const title = 'Reviews per state and day of a year';
+  const title = 'Reviews per state/cuisine and day of a year';
   
   const xValue = d => d.day;
   const xAxisLabel = 'Days';
@@ -50,25 +50,27 @@ const render = data => {
     .range([innerHeight, 0])
     .nice();
   
-  const colorScale = scaleOrdinal(schemeCategory10);
+  const colorScale = scaleOrdinal().range(["#AFC52F", "#ff6600", "#2a2fd4"]);
+  //let indata = [{name: infodicts[0][0], axes: first_vals, color: '#26AF32'},{name: infodicts[1][0], axes: second_vals, color: '#762712'}];
+
   
   const g = line_svg.append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
   
   const xAxis = axisBottom(xScale)
     .tickSize(-innerHeight)
-    .tickPadding(15);
+    .tickPadding(7);
   
   const yAxis = axisLeft(yScale)
     .tickSize(-innerWidth)
-    .tickPadding(10);
+    .tickPadding(7);
   
   const yAxisG = g.append('g').call(yAxis);
   yAxisG.selectAll('.domain').remove();
   
   yAxisG.append('text')
       .attr('class', 'axis-label')
-      .attr('y', -95)
+      .attr('y', -35)
       .attr('x', -innerHeight / 2)
       .attr('fill', 'black')
       .attr('transform', `rotate(-90)`)
@@ -82,7 +84,7 @@ const render = data => {
   
   xAxisG.append('text')
       .attr('class', 'axis-label')
-      .attr('y', 70)
+      .attr('y', 35)
       .attr('x', innerWidth / 2)
       .attr('fill', 'black')
       .text(xAxisLabel);
@@ -109,8 +111,8 @@ const render = data => {
   const nested = nest()
   	.key(colorValue)
   	.entries(data)
-  	.sort((a,b) => 
-          descending(lastYValue(a), lastYValue(b)));
+  	/*.sort((a,b) => 
+          descending(lastYValue(a), lastYValue(b)));*/
   
   colorScale.domain(nested.map(d => d.key));
   
@@ -120,21 +122,73 @@ const render = data => {
   		.attr('d', d => lineGenerator(d.values))
   		.attr('stroke', d => colorScale(d.key))
   	.append('title')
-  		.text(d => 'No. of reviews of ' + d.key + ': ' + d.values[0].review);
+      .text(d => 'No. of reviews of ' + d.key + ': ' + d.values.review);
+  
+  /*const focus = g.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+  focus.append("circle")
+      .attr("r", 5);
+
+  const overlay = g.append("rect")
+      .attr("class", "overlay")
+      .attr("width", 100)
+      .attr("height", 50)
+      .attr("x", 10)
+      .attr("y", -22)
+      .attr("rx", 4)
+      .attr("ry", 4);
+
+  focus.append("text")
+      .attr("class", "tooltip-date")
+      .attr("x", 18)
+      .attr("y", -2);
+
+  focus.append("text")
+      .attr("x", 18)
+      .attr("y", 18)
+      .text("Reviews:");
+
+  focus.append("text")
+      .attr("class", "tooltip-reviews")
+      .attr("x", 60)
+      .attr("y", 18);
+
+  svg.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+  function mousemove() {
+      const x0 = xValue.invert(mouse(this)[0]),
+          i = data, x0, 1),
+          d0 = data[i - 1],
+          d1 = data[i],
+          d = x0 - d0.day > d1.day - x0 ? d1 : d0;
+      focus.attr("transform", "translate(" + x(d.day) + "," + y(d.review) + ")");
+      focus.select(".tooltip-date").text(d.day);
+      focus.select(".tooltip-reviews").text(d.review);
+  }*/
 
   g.append('text')
       .attr('class', 'title')
       .attr('y', -10)
+      .attr('x', 140)
       .text(title);
 
   line_svg.append('g')
-      .attr('transform', `translate(800,120)`)
+      .attr('transform', `translate(800,190)`)
       .call(colorLegend, {
         colorScale,
         circleRadius: 10,
         spacing: 25,
         textOffset: 14,
-    		onClick,
+        onClick,
+        focus,
     		selectedStateValue
       });
 };
